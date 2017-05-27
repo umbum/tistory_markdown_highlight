a46acada11a92ec6c0dcf6f578fb4d991a3f50f3
umbum <umbum7601@gmail.com>
2017-05-27 10:26:01 +0000

    available inner tag and &

--- backquote_highlight.js
+++ backquote_highlight.js
@@ -1,32 +1,35 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var lines = $('div.article > div p, div.article > div div:not(:has(div))');
     var re = /``(?:([0-9a-z\-]*)\s)?.*?``/g;
-    for (var i = 0; i < lines.length; i++){
-        if( lines[i].innerText.startsWith('```')){
-            for ( var j = i+1; j < lines.length; j++){
- 
-                if (lines[j].innerText.startsWith('```')){
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].innerText.startsWith('```')) {
+
+            for (var j = i + 1; j < lines.length; j++) {
+
+                if (lines[j].innerText.startsWith('```')) {
                     wrapping(lines, i, j);
                     i = j;
                     break;
                 }
             }
         } else {
-            var line_buf = lines[i].innerText;
+            /*innerHTML로 받으면 내부에 <pre> 등 강제 개행 태그가 있을 때
+            자동으로 개행하게 되어 제대로 적용되지 않지만, <p> 또는 leaf node인 <div>에 <pre>가
+            있을 가능성은 희박하기 때문에 innerHTML로 처리했다.
+            innerHTML로 받으면 <b>, <s>, <i> 등을 적용할 수 있다는 장점이 있다.*/
+            var line_buf = lines[i].innerHTML;
             var matches = re.exec(line_buf);
-
-            while(matches){
+            while (matches) {
                 var k = '';
                 var lang = '';
                 var start_idx = 2;
-                if(matches[1]){
+                if (matches[1]) {
                     lang += matches[1].substr(0, 12);
-                    start_idx += matches[1].length+1;
+                    start_idx += matches[1].length + 1;
                 }
 
-                var escaped = matches[0].replace('<', '&lt;').replace('>', '&gt;');
-                var code = '<code class="language-'+lang+'">'+escaped.substring(start_idx, escaped.length-2)+'</code>';
-                lines[i].innerHTML = lines[i].innerHTML.replace(escaped, code);
+                var code = '<code class="language-' + lang + '">' + matches[0].substring(start_idx, matches[0].length - 2) + '</code>';
+                lines[i].innerHTML = lines[i].innerHTML.replace(matches[0], code);
 
                 matches = re.exec(line_buf);
             }
@@ -34,17 +37,18 @@ $(document).ready(function () {
     }
 });
 
-function wrapping(lines, i, j){
+function wrapping(lines, i, j) {
     var lang = lines[i].innerText.substr(3, 15)
-    var div_pre_code = $('<div><pre><code class="language-'+lang+'"></code></pre></div>');
+    var div_pre_code = $('<div><pre><code class="language-' + lang + '"></code></pre></div>');
     var code = $(div_pre_code).children().children();
 
-    for(var k = i+1; k < j; k++){
-        code.append(lines[k].innerHTML+"\n");
-				$(lines[k]).remove();
+    for (var k = i + 1; k < j; k++) {
+        code.append(lines[k].innerHTML + "\n");
+        $(lines[k]).remove();
     }
     $(lines[i]).after(div_pre_code);
-	  $(lines[i]).remove();
+    $(lines[i]).remove();
     $(lines[j]).remove();
 
 }
+
